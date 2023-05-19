6691cf600278b9b3034cd9222268eaae3db0b563
이치훈 <dlclgns152615@gmail.com>
2023-05-19 20:18:11 +0900

    [Solve] 수 정렬하기 2

---
+++ BOJ/2751/2751/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  2751
+//
+//  Created by 이치훈 on 2023/05/19.
+//
+
+let length = Int(String(readLine()!))!
+var nums = [Int]()
+
+for _ in 1...length {
+    let item = Int(String(readLine()!))!
+    nums.append(item)
+}
+
+nums.sort()
+
+for i in 0...nums.count - 1 {
+    print(nums[i])
+}
