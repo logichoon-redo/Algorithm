532352058daa957b0fb77b501bdffc5bdc5d71d6
이치훈 <dlclgns152615@gmail.com>
2023-05-27 20:21:13 +0900

    [Solve] 합

---
+++ BOJ/8393/8393/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  8393
+//
+//  Created by 이치훈 on 2023/05/27.
+//
+
+let num = Int(String(readLine()!))!
+var result = 0
+
+for i in 1...num {
+    result += i
+}
+print(result)
