c965c0e0fdf9b8a736d55c1dfd1c656f13721d04
logicHoon <dlclgns152615@gmail.com>
2023-07-01 17:23:10 +0900

    [Solve] 코딩은 체육과목 입니다

---
+++ BOJ/25314/25314/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  25314
+//
+//  Created by 이치훈 on 2023/07/01.
+//
+
+let length = Int(String(readLine()!))! / 4
+
+for _ in 1...length {
+  print("long", terminator: " ")
+}
+print("int")
