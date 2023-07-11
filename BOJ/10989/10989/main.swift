d578c4a8b7335cf610e58bd03fee307745afdae7
logicHoon <dlclgns152615@gmail.com>
2023-07-11 21:01:05 +0900

    [Solve] 수 정렬하기 3

---
+++ BOJ/10989/10989/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  10989
+//
+//  Created by 이치훈 on 2023/07/11.
+//
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+            && now != 32 && now != 0 {
+                str += String(bytes: [now], encoding: .ascii)!
+                now = read()
+        }
+
+        return str
+    }
+}
+
+let file = FileIO()
+let length = file.readInt()
+var nums = [Int](repeating: 0, count: 10001)
+var result = ""
+
+for _ in 0..<length {
+  let i = file.readInt()
+  nums[i] += 1
+}
+
+for i in 1...10000 {
+  result += String(repeating: "\(i)\n", count: nums[i])
+}
+
+print(result)
