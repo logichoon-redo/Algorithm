744aa68588414f800bd15a2005a9d3340f925fb3
logicHoon <dlclgns152615@gmail.com>
2023-07-26 00:12:26 +0900

    [Style] rename

--- BOJ/1406/1406/main.swift
+++ BOJ/1406/1406/main.swift
@@ -5,66 +5,35 @@
 //  Created by 이치훈 on 2023/07/25.
 //
 
-//var stackLeft = Array(readLine()!)
-//var stackRight = [Character]()
-//let m = Int(String(readLine()!))!
-//
-//for _ in 1...m {
-//  let command = readLine()!.split(separator: " ").map {
-//    Character(String($0))
-//  }
-//
-//  switch command[0] {
-//  case "L":
-//    if !stackLeft.isEmpty {
-//      stackRight.append(stackLeft.removeLast())
-//    }
-//  case "D":
-//    if !stackRight.isEmpty {
-//      stackLeft.append(stackRight.removeLast())
-//    }
-//  case "B":
-//    if !stackLeft.isEmpty {
-//      stackLeft.removeLast()
-//    }
-//  case "P":
-//    stackLeft.append(command[1])
-//  default:
-//    break
-//  }
-//}
-//
-//print(String(stackLeft + stackRight.reversed()))
-
 if let str = readLine() {
-    let n = readLine().map{Int($0)!}!
-
-    var stackL = str
-    var stackR = ""
-    
-    for _ in 0..<n {
-        var order = readLine()
-        switch order {
-        case "L":
-            if stackL != "" {
-                let c = stackL.removeLast()
-                stackR.append(c)
-            }
-        case "D":
-            if stackR != "" {
-                let c = stackR.removeLast()
-                stackL.append(c)
-            }
-        case "B":
-            if stackL != "" {
-                stackL.removeLast()
-            }
-        default:
-            let p = order!.removeLast()
-            stackL.append(p)
-        }
+  let n = readLine().map{Int($0)!}!
+  
+  var stackLeft = str
+  var stackRight = ""
+  
+  for _ in 0..<n {
+    var order = readLine()
+    switch order {
+    case "L":
+      if stackLeft != "" {
+        let c = stackLeft.removeLast()
+        stackRight.append(c)
+      }
+    case "D":
+      if stackRight != "" {
+        let c = stackRight.removeLast()
+        stackLeft.append(c)
+      }
+    case "B":
+      if stackLeft != "" {
+        stackLeft.removeLast()
+      }
+    default:
+      let p = order!.removeLast()
+      stackLeft.append(p)
     }
-    
-    print(stackL + stackR.reversed())
-    
+  }
+  
+  print(stackLeft + stackRight.reversed())
+  
 }
