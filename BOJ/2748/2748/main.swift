242a822aee52082fbbd4db190670887a91a3785e
logicHoon <dlclgns152615@gmail.com>
2023-09-09 23:40:58 +0900

    [Solve] 피보나치 수 2

---
+++ BOJ/2748/2748/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  2748
+//
+//  Created by 이치훈 on 2023/09/09.
+//
+
+let n = Int(String(readLine()!))!
+var dp = Array(repeating: 0, count: 91)
+dp[1] = 1
+dp[2] = 1
+
+for i in stride(from: 3, through: n, by: 1) {
+  dp[i] = dp[i-1] + dp[i-2]
+}
+
+print(dp[n])
