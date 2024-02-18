b770f755ca1ec28087065f836eb613bb6725d0e0
logicHoon <dlclgns152615@gmail.com>
2024-02-18 14:13:50 +0900

    [Solve] 문자열 폭발

---
+++ BOJ/9935/9935/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  9935
+//
+//  Created by 이치훈 on 2/18/24.
+//
+
+let input = readLine()!
+let bomb = readLine()!
+var stack = [Character]()
+
+for s in input {
+  stack.append(s)
+  
+  if s == bomb.last! {
+    
+    if stack.count >= bomb.count && String(stack[(stack.count - bomb.count)...]) == bomb {
+      _=(1...bomb.count).map { _ in
+        stack.popLast()
+      }r
+    }
+  }
+}
+
+stack.isEmpty ? print("FRULA") : print(String(stack))
