4a267aa8d0fc1c5b47052d9a1c86f236233d8d25
logicHoon <dlclgns152615@gmail.com>
2024-03-05 16:39:04 +0900

    [Solve] 행복 유치원

---
+++ BOJ/13164/13164/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  13164
+//
+//  Created by 이치훈 on 3/5/24.
+//
+
+let nk = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = nk[0], k = nk[1]
+let heights = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted()
+var heightDieffrences = [Int]()
+var answer = 0
+
+_=(0..<n-1).map { i in
+  heightDieffrences.append(heights[i+1] - heights[i])
+}
+heightDieffrences.sort()
+
+_=(0..<n-k).map { i in
+  answer += heightDieffrences[i]
+}
+
+print(answer)
