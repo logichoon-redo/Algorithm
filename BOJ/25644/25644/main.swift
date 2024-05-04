7e10b8985836a97cf438b74897fe7e521f080fb9
logicHoon <dlclgns152615@gmail.com>
2024-05-04 22:30:55 +0900

    [Solved] 최대 상승

--- BOJ/25644/25644/main.swift
+++ BOJ/25644/25644/main.swift
@@ -5,7 +5,14 @@
 //  Created by 이치훈 on 5/4/24.
 //
 
-import Foundation
+let n = Int(readLine()!)!
+let stock = readLine()!.split(separator: " ").map { Int(String($0))! }
+var dp = [Int](repeating: 0, count: n)
+var minStock = stock[0]
 
-print("Hello, World!")
+_=(1..<n).map { i in
+  minStock = min(minStock, stock[i])
+  dp[i] = max(dp[i-1], stock[i] - minStock)
+}
 
+print(dp.last!)
