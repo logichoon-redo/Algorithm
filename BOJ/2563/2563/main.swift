ee90826e37c37a6b980c5df0cd0c3abcca131f98
logicHoon <dlclgns152615@gmail.com>
2025-04-01 19:47:49 +0900

    [Solved] 색종이

---
+++ BOJ/2563/2563/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  2563
+//
+//  Created by 이치훈 on 4/1/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- BOJ/7568/7568/main.swift
+++ BOJ/7568/7568/main.swift
@@ -5,7 +5,22 @@
 //  Created by 이치훈 on 4/1/25.
 //
 
-import Foundation
+let n = Int(readLine()!)!
+var arr: [(Int, Int)] = []
+var result: [Int] = [Int](repeating: 1, count: n)
 
-print("Hello, World!")
+for _ in 0..<n {
+  let input = readLine()!.split(separator: " ").map { Int($0)! }
+  arr.append((input[0], input[1]))
+}
 
+for i in 0..<n {
+  let target = arr[i]
+  _=arr.map { j in
+    if target.0 < j.0 && target.1 < j.1 {
+      result[i] += 1
+    }
+  }
+}
+
+print(result.map {String($0)}.joined(separator: " "))
