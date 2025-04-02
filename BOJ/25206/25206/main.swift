cbe69bdfe963bfa23700d826dcfee72be464ee8d
logicHoon <dlclgns152615@gmail.com>
2025-04-02 23:52:32 +0900

    [Solved] 너의 평점은

---
+++ BOJ/25206/25206/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  25206
+//
+//  Created by 이치훈 on 4/2/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- BOJ/2563/2563/main.swift
+++ BOJ/2563/2563/main.swift
@@ -5,7 +5,18 @@
 //  Created by 이치훈 on 4/1/25.
 //
 
-import Foundation
+let n = Int(readLine()!)!
+var paper = [Int]()
+var backBoard = [[Int]](repeating: [Int](repeating: 0, count: 100), count: 100)
 
-print("Hello, World!")
+for _ in 0..<n {
+  let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+  
+  for i in input[0]..<input[0]+10 {
+    for j in input[1]..<input[1]+10 {
+      backBoard[i][j] = 1
+    }
+  }
+}
 
+print(backBoard.map { $0.reduce(0) { $0 + $1 } }.reduce(0) { $0 + $1 })
