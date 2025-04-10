bf45c2cb803d21d7e7bcfed78f7133e0a69c456f
logicHoon <dlclgns152615@gmail.com>
2025-04-10 14:40:56 +0900

    [Solved] 재귀함수가 뭔가요?

---
+++ BOJ/17478/17478/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  17478
+//
+//  Created by 이치훈 on 4/10/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
--- BOJ/25206/25206/main.swift
+++ BOJ/25206/25206/main.swift
@@ -5,7 +5,38 @@
 //  Created by 이치훈 on 4/2/25.
 //
 
-import Foundation
+var scoreSum: Double = 0.0
+var sum: Double = 0.0
 
-print("Hello, World!")
+for _ in 1...20 {
+  let subject = readLine()!.split(separator: " ").map{ String($0) }
+  
+  switch subject[2] {
+  case "A+":
+    scoreSum += Double(subject[1])! * 4.5
+  case "A0":
+    scoreSum += Double(subject[1])! * 4.0
+  case "B+":
+    scoreSum += Double(subject[1])! * 3.5
+  case "B0":
+    scoreSum += Double(subject[1])! * 3.0
+  case "C+":
+    scoreSum += Double(subject[1])! * 2.5
+  case "C0":
+    scoreSum += Double(subject[1])! * 2.0
+  case "D+":
+    scoreSum += Double(subject[1])! * 1.5
+  case "D0":
+    scoreSum += Double(subject[1])! * 1.0
+  case "F":
+    scoreSum += Double(subject[1])! * 0.0
+  default:
+    break
+  }
+  
+  if subject[2] != "P" {
+    sum += Double(subject[1])!
+  }
+}
 
+print(scoreSum / sum)
