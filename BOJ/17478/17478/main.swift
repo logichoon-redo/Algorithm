9c93b12a6c231812c7d008ab339fcde5914fe9fe
logicHoon <dlclgns152615@gmail.com>
2025-04-15 03:03:11 +0900

    [Solved] 에라토스테네스의 체

--- BOJ/17478/17478/main.swift
+++ BOJ/17478/17478/main.swift
@@ -5,7 +5,39 @@
 //  Created by 이치훈 on 4/10/25.
 //
 
-import Foundation
+let n = Int(readLine()!)!
 
-print("Hello, World!")
+func printUnderBar(count: Int) {
+  for _ in 0..<count*4 {
+    print("_",terminator: "")
+  }
+}
+
+func story(_ localN: Int) {
+  if localN > 0 {
+    printUnderBar(count: n - localN)
+    print("\"재귀함수가 뭔가요?\"")
+    printUnderBar(count: n - localN)
+    print("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.")
+    printUnderBar(count: n - localN)
+    print("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
+    printUnderBar(count: n - localN)
+    print("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
+    story(localN-1)
+  } else if localN == 0 {
+    printUnderBar(count: n - localN)
+    print("\"재귀함수가 뭔가요?\"")
+    printUnderBar(count: n - localN)
+    print("\"재귀함수는 자기 자신을 호출하는 함수라네\"")
+    printUnderBar(count: n - localN)
+    print("라고 답변하였지.")
+    return
+  }
+  
+  printUnderBar(count: n - localN)
+  print("라고 답변하였지.")
+}
+
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+story(n)
 
---
+++ BOJ/2960/2960/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  2960
+//
+//  Created by 이치훈 on 4/11/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
